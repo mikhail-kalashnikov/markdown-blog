b03900d9932a6067e24ca1ae352f272980aabd35
HoangVv-uet <hoangk62uet@gmail.com>
2020-06-06 14:23:41 +0700

    final version 1.0,0

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const app = express();
+const Article = require('./models/article');
+const methodOverride = require('method-override');
 const port = process.env.PORT || 9999;
+require('./config/mongoose');
+
 app.set('view engine', 'ejs');
-app.use('/articles', require('./routes/articles'));
-app.get('/', (req, res) => {
-  const articles = [
-    {
-      title: 'Test Articles',
-      createdAt: new Date(),
-      description: 'This is a article',
-    },
-    {
-      title: 'Test Articles 1',
-      createdAt: new Date(),
-      description: 'This is a article',
-    },
-  ];
+app.use(express.urlencoded());
+app.use(methodOverride('_method'));
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createAt: 'desc' });
   res.render('articles/index.ejs', { articles: articles });
 });
+app.use('/articles', require('./routes/articles'));
+
 app.use('*', function (req, res, next) {
   res.send('404 page');
 });
+
 app.listen(port, () => {
   console.log('server listening on ' + port);
 });
---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://localhost:27017/markdown-blog';
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
