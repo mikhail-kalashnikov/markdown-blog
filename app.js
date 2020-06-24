578a71484803ef0cee7f26a3b0ecb2d627d49568
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-06-24 20:34:51 +0700

    setup eslint

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Article = require('./models/article');
-const methodOverride = require('method-override');
+const Article = require("./models/article");
+const methodOverride = require("method-override");
 const port = process.env.PORT || 9999;
-require('./config/mongoose');
-
-app.set('view engine', 'ejs');
+require("./config/mongoose");
+app.set("view engine", "ejs");
 app.use(express.urlencoded());
-app.use(methodOverride('_method'));
-app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createAt: 'desc' });
-  res.render('articles/index.ejs', { articles: articles });
+app.use(methodOverride("_method"));
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createAt: "desc" });
+  res.render("articles/index.ejs", { articles: articles });
 });
-app.use('/articles', require('./routes/articles'));
+app.use("/articles", require("./routes/articles"));
 
-app.use('*', function (req, res, next) {
-  res.send('404 page');
+app.use("*", function (req, res, next) {
+  res.send("404 page");
 });
 app.listen(port, () => {
-  console.log('server listening on ' + port);
+  console.log("server listening on " + port);
 });
